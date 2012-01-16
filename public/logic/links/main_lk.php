1de3e3b70a2e0f41cb66a6bc4eb53645392a16fb
willn <willn@umich.edu>
2012-01-16 03:30:58 +0000

    remove error log debugging

--- public/logic/links/main_lk.php
+++ public/logic/links/main_lk.php
@@ -6,7 +6,6 @@ global $mysql_api;
 
 $sql = 'select cid, cmty from committees where parent=cid or parent=0 order by parent';
 $CInfo = $mysql_api->get($sql, 'cid');
-error_log("cinfo: " . print_r($CInfo, true ));
 $Cmtys = array( );
 foreach( $CInfo as $i=>$Info )
 { $Cmtys[$Info['cid']] = $Info['cmty']; }
